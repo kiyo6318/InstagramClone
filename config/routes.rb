d13867618de3c946ff4049d2ffa2bbd118b1e364
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-11 15:22:50 +0900

    g controller実行 index,new,edit,confirmアクションとビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'picture/index'
-  get 'picture/new'
-  get 'picture/edit'
-  get 'picture/confirm'
+  get 'pictures/index'
+  get 'pictures/new'
+  get 'pictures/edit'
+  get 'pictures/confirm'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
