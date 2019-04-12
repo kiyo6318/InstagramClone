09827c94c5d1248f8e0e6ac0691e75395c62a935
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-12 16:34:23 +0900

    picture投稿機能を実装した

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,15 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
+    @picture = Picture.new
   end
 
   def create
+    @picture = Picture.new(picture_params)
+    redirect_to pictures_path
   end
 
   def edit
@@ -18,5 +22,11 @@ class PicturesController < ApplicationController
   end
 
   def confirm
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:word,:image)
   end
 end
