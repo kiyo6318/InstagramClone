2cf550c5ef586b64e6e8ef9f61a1b55795c1cc85
前川清成 <kiyonari6318@MacbookPro.local>
2019-04-15 18:11:46 +0900

    ユーザー詳細ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_new_path, notice: "アカウントを作成しました!"
+      redirect_to user_path(@user.id), notice: "アカウントを作成しました!"
     else
       render'new'
     end
